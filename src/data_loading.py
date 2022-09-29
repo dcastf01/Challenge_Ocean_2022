44e3814b75e8eba458ada000f7f023e0ddcc4205
dcastf <dcastf01@gmail.com>
2022-09-29 19:00:51 +0200

    First commit

---
+++ src/data_loading.py
@@ -0,0 +1,9 @@
+from audio import load_audio
+
+
+
+path=r'D:\programacion\Repositorios\Challenge_Ocean_2022\data\LibriSpeech\dev-clean\84\121123\84-121123-0000.flac'
+
+audio=load_audio(path)
+
+print(audio)
