99ee68e88a8d2104ab8714df976326f09e64db23
dcastf <dcastf01@gmail.com>
2022-09-29 20:02:42 +0200

    changing folder and add utils

--- src/utils.py
+++ src/utils.py
@@ -85,3 +85,14 @@ def write_srt(transcript: Iterator[dict], file: TextIO):
             file=file,
             flush=True,
         )
+
+def play_audio(waveform, sample_rate):
+  waveform = waveform.numpy()
+
+  num_channels, num_frames = waveform.shape
+  if num_channels == 1:
+    display(Audio(waveform[0], rate=sample_rate))
+  elif num_channels == 2:
+    display(Audio((waveform[0], waveform[1]), rate=sample_rate))
+  else:
+    raise ValueError("Waveform with more than 2 channels are not supported.")
